76d8d984ac0221f776a5bac3531f8ad8cd2a6a2b
pavel <smvynt@gmail.com>
2025-09-19 11:57:14 +0200

    ex06

---
+++ ex06/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psmolin <psmolin@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 12:51:32 by psmolin           #+#    #+#             */
+/*   Updated: 2025/09/19 11:55:37 by psmolin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(int ac, char **av)
+{
+	Harl	harl;
+	if (ac != 2)
+	{
+		std::cerr << "Please, provide a level." << std::endl;
+		return (1);
+	}
+	harl.complain(av[1]);
+	return (0);
+}
+
